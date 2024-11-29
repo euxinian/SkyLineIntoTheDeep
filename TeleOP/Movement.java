db12177e505b700932bab0544208c75d2004f457
Calin <chelcea.calin@yahoo.com>
2024-11-29 22:46:31 +0200

    shit done

--- TeleOP/Movement.java
+++ TeleOP/Movement.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Teleop;
+package org.firstinspires.ftc.teamcode.TeleOP;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,16 +8,30 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name="TeleOP", group="Iterative Opmode")
 public class Movement extends OpMode
 {
-    private DcMotor LF = null;
-    private DcMotor RF = null;
-    private DcMotor LR = null;
-    private DcMotor RR = null;
-    private Servo Gheara = null;
-    private DcMotor armExtendMotor = null;
+    private DcMotor LF, RF, LR, RR, armExtendMotor;
+    private Servo Gheara;
+
+    private static final double DEADZONE = 0.05;
+    private static final double GHEARA_OPEN_POSITION = 0.16;
+    private static final double GHEARA_CLOSED_POSITION = 0.115;
+
+    public enum DirectionEnum {
+        FORWARD, REVERSE
+    }
+
+    public void initControl(DcMotor dcMotor, DirectionEnum direction) {
+        switch (direction) {
+            case FORWARD:
+                dcMotor.setDirection(DcMotor.Direction.FORWARD);
+            default:
+                dcMotor.setDirection(DcMotor.Direction.REVERSE);
+        }
+        dcMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        dcMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
 
     @Override
     public void init(){
-
         LF = hardwareMap.get(DcMotor.class, "left_front");
         RF = hardwareMap.get(DcMotor.class, "right_front");
         LR = hardwareMap.get(DcMotor.class, "left_rear");
@@ -25,21 +39,10 @@ public class Movement extends OpMode
         Gheara = hardwareMap.get(Servo.class, "claw_servo");
         armExtendMotor = hardwareMap.get(DcMotor.class, "arm_extend_motor");
 
-        LF.setDirection(DcMotor.Direction.FORWARD);
-        RF.setDirection(DcMotor.Direction.REVERSE);
-        LR.setDirection(DcMotor.Direction.FORWARD);
-        RR.setDirection(DcMotor.Direction.REVERSE);
-
-        LF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        RF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        LR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        RR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        LF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        RF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        LR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        RR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        initControl(LF, DirectionEnum.FORWARD);
+        initControl(RF, DirectionEnum.REVERSE);
+        initControl(LR, DirectionEnum.FORWARD);
+        initControl(RR, DirectionEnum.REVERSE);
         Gheara.setPosition(0);
     }
 
@@ -48,31 +51,26 @@ public class Movement extends OpMode
 
         double x = gamepad1.left_stick_x;
         double y = -gamepad1.left_stick_y;
+        double rotire = gamepad1.right_stick_x;
+
         boolean button_a = gamepad1.a;
         boolean button_b = gamepad1.b;
 
-        double rotire = gamepad1.right_stick_x;
         double alfa = Math.atan2(y, x);
         double pow = Math.hypot(y, x);
 
         double sin = Math.sin(alfa - Math.PI/4);
         double cos = Math.cos(alfa - Math.PI/4);
-        double max;
 
-        if(Math.abs(y)<0.05 && Math.abs(x)<0.05 && Math.abs(-rotire)<0.05){
-            LF.setPower(0);
-            RF.setPower(0);
-            LR.setPower(0);
-            RR.setPower(0);
+        if (Math.abs(x) < DEADZONE && Math.abs(y) < DEADZONE && Math.abs(rotire) < DEADZONE) {
+            stopMotors();
         } else{
             double PUTERE_LF = y + x + rotire;
             double PUTERE_RF = y - x - rotire;
             double PUTERE_LR = y - x + rotire;
             double PUTERE_RR = y + x - rotire;
 
-            max = Math.max(Math.abs(PUTERE_LF), Math.abs(PUTERE_RF));
-            max = Math.max(max, Math.abs(PUTERE_LR));
-            max = Math.max(max, Math.abs(PUTERE_RR));
+            double max = Math.max(Math.abs(PUTERE_LF), Math.max(Math.abs(PUTERE_RF), Math.max(Math.abs(PUTERE_LR), Math.abs(PUTERE_RR))));
 
             if(max > 1.0){
                 PUTERE_LF /= max;
@@ -81,6 +79,7 @@ public class Movement extends OpMode
                 PUTERE_RR /= max;
             }
 
+            // Set motor powers
             LF.setPower(PUTERE_LF);
             RF.setPower(PUTERE_RF);
             LR.setPower(PUTERE_LR);
@@ -88,28 +87,29 @@ public class Movement extends OpMode
         }
 
         if(button_a){
-            Gheara.setPosition(0.16);
+            Gheara.setPosition(GHEARA_OPEN_POSITION);
         }
         if(button_b){
-            Gheara.setPosition(0.115);
+            Gheara.setPosition(GHEARA_CLOSED_POSITION);
         }
 
-        int armPos=armExtendMotor.getCurrentPosition();
+        int armPos = armExtendMotor.getCurrentPosition();
         double armPower = -gamepad2.right_stick_y/2;
-        if (armPower>0 && armPos<1000)
+
+        if (Math.abs(armPower) > DEADZONE && (armPower > 0 && armPos < 1000 || armPower < 0 && armPos > 2)) {
             armExtendMotor.setPower(armPower);
-        else if (armPower<0 && armPos>2)
-            armExtendMotor.setPower(armPower);
+        }
         else{
             armPower=0;
         }
-
-
-
-
     }
 }
 
-
+void stopMotors() {
+    LF.setPower(0);
+    RF.setPower(0);
+    LR.setPower(0);
+    RR.setPower(0);
+}
 // Discord @euxinian
 // Bagati si voi discordurile :)))
